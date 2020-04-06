0a16fda8e79ca12514fceccf0857aa15f95364a3
ajay01101999 <60016332+ajay01101999@users.noreply.github.com>
2020-04-07 01:03:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Resume from './container/resume';
+import axios from 'axios';
+     
+    return (
+      <div>
+        <Resume/>
+      </div>
+    )
+
+// Project Console: https://console.firebase.google.com/project/basic-firebase-307b9/overview
+// Hosting URL: https://basic-firebase-307b9.firebaseapp.com
+
+export default App;
